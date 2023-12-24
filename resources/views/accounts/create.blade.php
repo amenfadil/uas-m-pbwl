16a137660c770eb2aa1e91c3545ea3272c14d065
amenfadil <123435143+amenfadil@users.noreply.github.com>
2023-12-25 00:48:32 +0700

    tugas kelar

--- resources/views/accounts/create.blade.php
+++ resources/views/accounts/create.blade.php
@@ -5,30 +5,30 @@
 @section('content')
     <div class="row">
         <div class="col-md-6">
-            <h2>Buat Akun Baru</h2>
+            <h2 style="color: #28a745; margin-bottom: 20px;">Buat Akun Baru</h2>
             <form action="{{ route('accounts.store') }}" method="POST">
                 @csrf
                 <div class="form-group">
-                    <label for="id">ID Akun:</label>
-                    <input type="text" name="id" class="form-control" maxlength="16" oninput="this.value = this.value.toUpperCase();" required>
+                    <label for="id" style="color: #343a40;">ID Akun:</label>
+                    <input type="text" name="id" class="form-control" maxlength="16" oninput="this.value = this.value.toUpperCase();" style="background-color: #343a40; color: white;" required>
                 </div>
                 <div class="form-group">
-                    <label for="nama">Nama:</label>
-                    <input type="text" name="nama" class="form-control" maxlength="255" oninput="this.value = this.value.toUpperCase();" required>
+                    <label for="nama" style="color: #343a40;">Nama:</label>
+                    <input type="text" name="nama" class="form-control" maxlength="255" oninput="this.value = this.value.toUpperCase();" style="background-color: #343a40; color: white;" required>
                 </div>
                 <div class="form-group">
-                    <label>Jenis:</label><br>
+                    <label style="color: #343a40;">Jenis:</label><br>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="jenis" id="bisnis" value="BISNIS" required>
-                        <label class="form-check-label" for="bisnis">BISNIS</label>
+                        <input class="form-check-input" type="radio" name="jenis" id="bisnis" value="BISNIS" style="background-color: #343a40; color: white;" required>
+                        <label class="form-check-label" for="bisnis" style="color: #343a40;">BISNIS</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="jenis" id="personal" value="PERSONAL" required>
-                        <label class="form-check-label" for="personal">PERSONAL</label>
+                        <input class="form-check-input" type="radio" name="jenis" id="personal" value="PERSONAL" style="background-color: #343a40; color: white;" required>
+                        <label class="form-check-label" for="personal" style="color: #343a40;">PERSONAL</label>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Create</button>
-                <a href="{{ route('accounts.index') }}" class="btn btn-secondary">Cancel</a>
+                <button type="submit" class="btn btn-success" style="background-color: #28a745; border: none;">Create</button>
+                <a href="{{ route('accounts.index') }}" class="btn btn-secondary" style="background-color: #6c757d; border: none;">Cancel</a>
             </form>
         </div>
     </div>
