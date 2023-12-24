6eaa200ee98ffa83b6240ba5b899f5a4f4086131
amenfadil <123435143+amenfadil@users.noreply.github.com>
2023-12-25 00:49:12 +0700

    tugas kelar

--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -5,24 +5,24 @@
 @section('content')
     <div class="row">
         <div class="col-md-6">
-            <h2>Edit Akun</h2>
+            <h2 style="color: #007bff; margin-bottom: 20px;">Edit Akun</h2>
             <form action="{{ route('accounts.update', $account->id) }}" method="POST">
                 @csrf
                 @method('PUT')
                 <div class="form-group">
-                    <label for="id">ID Akun:</label>
-                    <input type="text" name="id" class="form-control" value="{{ $account->id }}" maxlength="16" readonly required>
+                    <label for="id" style="color: #343a40;">ID Akun:</label>
+                    <input type="text" name="id" class="form-control" value="{{ $account->id }}" maxlength="16" readonly required style="background-color: #343a40; color: white;">
                 </div>
                 <div class="form-group">
-                    <label for="nama">Nama:</label>
-                    <input type="text" name="nama" class="form-control" value="{{ $account->nama }}" required>
+                    <label for="nama" style="color: #343a40;">Nama:</label>
+                    <input type="text" name="nama" class="form-control" value="{{ $account->nama }}" required style="background-color: #343a40; color: white;">
                 </div>
                 <div class="form-group">
-                    <label for="jenis">Jenis:</label>
-                    <input type="text" name="jenis" class="form-control" value="{{ $account->jenis }}" required>
+                    <label for="jenis" style="color: #343a40;">Jenis:</label>
+                    <input type="text" name="jenis" class="form-control" value="{{ $account->jenis }}" required style="background-color: #343a40; color: white;">
                 </div>
-                <button type="submit" class="btn btn-primary">Update</button>
-                <a href="{{ route('accounts.index') }}" class="btn btn-secondary">Batal</a>
+                <button type="submit" class="btn btn-success" style="background-color: #28a745; border: none;">Update</button>
+                <a href="{{ route('accounts.index') }}" class="btn btn-secondary" style="background-color: #6c757d; border: none;">Batal</a>
             </form>
         </div>
     </div>
